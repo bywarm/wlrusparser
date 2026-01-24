f0ebe1e286e1c79635454b59c081eee04b7e8c16
bywarm <warmixd@gmail.com>
2026-01-24 21:49:09 +0300

    Update simple_merge.py

--- scripts/simple_merge.py
+++ scripts/simple_merge.py
@@ -180,9 +180,9 @@ URLS = [
     "https://raw.githubusercontent.com/FalerChannel/FalerChannel/refs/heads/main/configs",
     "https://raw.githubusercontent.com/officialdakari/psychic-octo-tribble/refs/heads/main/subwl.txt",
     "https://raw.githubusercontent.com/RKPchannel/RKP_bypass_configs/refs/heads/main/configs",
-    "https://raw.githubusercontent.com/Ai123999/WhiteeListSub/refs/heads/main/whitelistkeys",
-    "https://raw.githubusercontent.com/EtoNeYaProject/etoneyaproject.github.io/refs/heads/main/2",
 
+    "https://raw.githubusercontent.com/EtoNeYaProject/etoneyaproject.github.io/refs/heads/main/2",
+    "https://s3c3.001.gpucloud.ru/dixsm/htxml",
 ]
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
